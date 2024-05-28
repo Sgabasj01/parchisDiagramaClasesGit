d6cc47ffe0c1f2f26f80b63b6aa54eba46588771
sabrina <sabrina@DESKTOP-RF07BVV>
2024-05-28 21:31:41 +0200

    Fase 4 - Añadir los metodos a la clase Ficha

--- parchisDiagramaClasesSabrina/src/main/java/parchisClasesPruebasUnits/Ficha.java
+++ parchisDiagramaClasesSabrina/src/main/java/parchisClasesPruebasUnits/Ficha.java
@@ -3,5 +3,20 @@ package parchisClasesPruebasUnits;
 public class Ficha {
 	
 	private int posicion;
+	
+	public Ficha(int posicion) {
+		
+        this.posicion = posicion;
+    }
+
+    public int getPosicion() {
+    	
+        return posicion;
+    }
+
+    public void setPosicion(int posicion) {
+    	
+        this.posicion = posicion;
+    }
 
 }
