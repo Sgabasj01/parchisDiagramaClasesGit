56777b0239a2ac79fc052ad9bbde4d19d16b6ed9
sabrina <sabrina@DESKTOP-RF07BVV>
2024-05-28 21:25:27 +0200

    Fase 2 - Añadir metodos a la clase Usuario

--- parchisDiagramaClasesSabrina/src/main/java/parchisClasesPruebasUnits/Usuario.java
+++ parchisDiagramaClasesSabrina/src/main/java/parchisClasesPruebasUnits/Usuario.java
@@ -5,5 +5,21 @@ public class Usuario {
 	private String nombreUsuario;
 	
 	private String email;
+	
+	public Usuario(String nombreUsuario, String email) {
+		
+	       this.nombreUsuario = nombreUsuario;
+	       this.email = email;
+	}
+
+    public String getNombreUsuario() {
+    	
+        return nombreUsuario;
+    }
+
+    public String getEmail() {
+    	
+        return email;
+    }
 
 }
